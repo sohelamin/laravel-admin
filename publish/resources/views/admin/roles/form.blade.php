72fcf721fad506307b0b42a229312d5dc4943792
Shipu Ahamed <shipuahamed01@gmail.com>
2017-10-22 13:38:59 +0600

    Undifine role fixed
    Undifine role fixed

--- publish/resources/views/admin/roles/form.blade.php
+++ publish/resources/views/admin/roles/form.blade.php
@@ -15,7 +15,7 @@
 <div class="form-group{{ $errors->has('label') ? ' has-error' : ''}}">
     {!! Form::label('label', 'Permissions: ', ['class' => 'col-md-4 control-label']) !!}
     <div class="col-md-6">
-        {!! Form::select('permissions[]', $permissions, $role->permissions->pluck('name'), ['class' => 'form-control', 'multiple' => true]) !!}
+        {!! Form::select('permissions[]', $permissions, isset($role) ? $role->permissions->pluck('name') : [], ['class' => 'form-control', 'multiple' => true]) !!}
         {!! $errors->first('label', '<p class="help-block">:message</p>') !!}
     </div>
 </div>
