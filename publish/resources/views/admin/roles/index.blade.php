e5defa190f09f35df152bb579c2c855cf65f2a48
Sohel Amin <sohelamincse@gmail.com>
2018-03-06 23:40:21 +0600

    Fix search button on roles

--- publish/resources/views/admin/roles/index.blade.php
+++ publish/resources/views/admin/roles/index.blade.php
@@ -17,7 +17,7 @@
                         <div class="input-group">
                             <input type="text" class="form-control" name="search" placeholder="Search...">
                             <span class="input-group-btn">
-                                <button class="btn btn-default" type="submit">
+                                <button class="btn btn-secondary" type="submit">
                                     <i class="fa fa-search"></i>
                                 </button>
                             </span>
